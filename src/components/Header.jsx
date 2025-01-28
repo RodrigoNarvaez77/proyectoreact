aa9ad6f4d916ba146975c020d143996c614787a1
RodrigoNarvaez77 <rodrigo.narvaez.munoz@ciisa.cl>
2025-01-28 16:15:09 -0300

    28-01-2025

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,26 +30,28 @@ const Header = () => {
         </a>
 
         {/* Botón para menú móvil */}
-        <button
-          className="lg:hidden text-gray-200 focus:outline-none"
-          onClick={toggleMenu}
-          aria-label="Toggle navigation"
-        >
-          <svg
-            className="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
+        <div className="fixed inset-0 bg-gray-900 bg-opacity-95 z-50 flex flex-col items-center justify-center space-y-6 text-white text-lg">
+          <button
+            className="lg:hidden text-gray-200 focus:outline-none"
+            onClick={toggleMenu}
+            aria-label="Toggle navigation"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M4 6h16M4 12h16m-7 6h7"
-            />
-          </svg>
-        </button>
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16m-7 6h7"
+              />
+            </svg>
+          </button>
+        </div>
 
         {/* Menú dinámico */}
         <div
